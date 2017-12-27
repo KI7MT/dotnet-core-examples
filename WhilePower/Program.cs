cefd4760c1d4b9d823917e102528e21d9841b7c0
ki7mt <ki7mt@yahoo.com>
2017-12-27 14:46:08 -0700

    Add While Power of 10 loop and comments

---
+++ WhilePower/Program.cs
@@ -0,0 +1,41 @@
+﻿/* 
+ *  Author ......: Greg, Beam, KI7MT, <ki7mt@yahoo.com>
+ *  Copyright ...: Copyright (C) 2017 GPLv3
+ *  Level .......: Basic
+ *  Description..: While loop that calculates powers of 10  for x < 6
+ */
+using System;
+
+namespace Beam.Example.WhilePower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // method variables
+            int x = 1;
+            double power10;
+
+            // print header
+            Console.WriteLine();
+            Console.WriteLine(" Values for ( 10 ^ x ) where ( x =< 10 )");
+            Console.WriteLine();
+
+            // start loop
+            while (x <= 10)
+            {
+               power10 = Math.Pow(10, x);
+               Console.WriteLine(" 10 to the power of {0} is {1}", x, power10.ToString("#,##0"));
+               x++;
+            }
+
+            // print footer
+            Console.WriteLine();
+            Console.WriteLine(" Press Any Key to Exit...");
+            Console.ReadKey();
+
+        } // END - Main Method
+
+    } // END - class Program
+
+} // END - namespace Beam.Example.WhilePower
