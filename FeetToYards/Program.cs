4c184140691f256496e1ff373ad4e9801a548d91
ki7mt <ki7mt@yahoo.com>
2017-12-26 22:14:19 -0700

    Add first Feet to Yards and git ignore

---
+++ FeetToYards/Program.cs
@@ -0,0 +1,34 @@
+﻿/* 
+ *  Author ......: Greg, Beam, KI7MT, <ki7mt@yahoo.com>
+ *  Copyright ...: Copyright (C) 2017 GPLv3
+ *  Level .......: Basic
+ *  Description..: User Input feet => convert feet to yards
+ */
+using System;
+
+namespace FeetToYards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double conversionFactor = 0.111111111;
+            double squareFeetIn;
+            double squareYardOut;
+
+            Console.WriteLine();
+            Console.Write(" Enter Square Feet ..: ");
+            squareFeetIn = Convert.ToDouble(Console.ReadLine());
+            squareYardOut = squareFeetIn * conversionFactor;
+
+            // print the data
+            Console.Write(" Square yards .......: {0}\n", squareYardOut);
+            Console.WriteLine();
+            Console.Write(" Press Any Key to Exit...");
+            Console.ReadKey();
+
+        } // END - Main Method
+    
+    } // END - class Program
+
+} // END - namespace FeetToYards
