2787475828d8cc2a0868a0203ecfa60b774c4142
ki7mt <ki7mt@yahoo.com>
2017-12-27 00:54:53 -0700

    Update code and comments for Smallest of Five numebrs example

--- SmallestOfFive/Program.cs
+++ SmallestOfFive/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/* 
+ *  Author ......: Greg, Beam, KI7MT, <ki7mt@yahoo.com>
+ *  Copyright ...: Copyright (C) 2017 GPLv3
+ *  Level .......: Basic
+ *  Description..: Determine the smallest of five numbers
+ */
+using System;
 
 namespace SmallestOfFive
 {
@@ -6,7 +12,63 @@ namespace SmallestOfFive
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+            // method variables
+            double num1;
+            double num2;
+            double num3;
+            double num4;
+            double num5;
+            double smallestNum;
+
+            // user input - No Input Validation
+            Console.WriteLine();
+            Console.WriteLine(" Find the smallest of five numbers\n");
+
+            Console.Write(" Enter first number ...: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write(" Enter second number ..: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write(" Enter third number ...: ");
+            num3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write(" Enter fourth number ..: ");
+            num4 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write(" Enter fifth number ...: ");
+            num5 = Convert.ToDouble(Console.ReadLine());
+
+            // logic to determien smallest number
+            if (num1 <= num2 && num1 <= num3 && num1 <= num4 && num1 <= num5) // num1 is smallest
+            {
+               smallestNum = num1;
+            }
+            else if (num2 <= num1 && num2 <= num3 && num2 <= num4 && num2 <= num5) // num2 is smallest
+            {
+               smallestNum = num2;
+            }
+            else if (num3 <= num1 && num3 <= num2 && num3 <= num4 && num3 <= num5) // num3 is smallest
+            {
+               smallestNum = num3;
+            }
+            else if (num4 <= num1 && num4 <= num2 && num4 <= num3 && num4 <= num5) // num4 is smallest
+            {
+               smallestNum = num4;
+            }
+            else
+            {
+               smallestNum = num5; // if not num1 thru num4, smallest must be num5
+            }
+
+            // print results
+            Console.WriteLine();
+            Console.WriteLine(" Smallest number is ...: {0}\n", smallestNum);
+            Console.Write(" Press Any Key to Exit...");
+            Console.ReadKey();
+
+        } // END - Main Method
+
+    } // END - class Program
+
+} // END - namespace Beam.Example.SmallestOfFive
