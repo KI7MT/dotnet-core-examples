0e3e52a88951bc84819d6b3bbc1f366178edc62c
ki7mt <ki7mt@yahoo.com>
2017-12-27 14:28:26 -0700

    Add While Cubed code and comments

---
+++ loop-examples/WhileCubed/Program.cs
@@ -0,0 +1,42 @@
+﻿/* 
+ *  Author ......: Greg, Beam, KI7MT, <ki7mt@yahoo.com>
+ *  Copyright ...: Copyright (C) 2017 GPLv3
+ *  Level .......: Basic
+ *  Description..: While loop to calculate cubed values
+ */
+using System;
+
+namespace Beam.Example.WhileCubed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // method variables
+            int x = 1;
+            double cubed;
+
+            // print header
+            Console.WriteLine();
+            Console.WriteLine(" Values for 1 thru 10 cubed");
+            Console.WriteLine();
+
+            // start loop
+            while (x <= 10)
+            {
+                cubed = Math.Pow(x, 3);
+                Console.WriteLine(" {0} cubed is {1}", x, cubed);
+                x++;
+            }
+
+            // print footer
+            Console.WriteLine();
+            Console.WriteLine(" Press Any Key to Exit...");
+            Console.ReadKey();
+
+        } // END - Main Method
+
+    } // END - class Program
+
+} // END - namespace Beam.Example.WhileCubed
+
