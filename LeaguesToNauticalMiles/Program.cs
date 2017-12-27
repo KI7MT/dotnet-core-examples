78649274918128e17588e573ed5df21453176611
ki7mt <ki7mt@yahoo.com>
2017-12-27 00:07:20 -0700

    Update comments Leagues to Nautical miles

--- LeaguesToNauticalMiles/Program.cs
+++ LeaguesToNauticalMiles/Program.cs
@@ -12,19 +12,27 @@ namespace Beam.Example.LeaguesToNauticalMiles
     {
         static void Main(string[] args)
         {
+            // method variables
             double conversionFactor = 3.0000;
             double leaIn;
             double nmOut;
 
+            // user input - No Input Validation
             Console.WriteLine();
             Console.Write(" Enter Number of Leagues ..: ");
             leaIn = Convert.ToDouble(Console.ReadLine());
+
+            // calculation
             nmOut = leaIn * conversionFactor;
 
+            // print result
             Console.Write(" Nautical Miles ...........: {0}\n", nmOut);
             Console.WriteLine();
             Console.Write(" Press Any Key To Exit...");
             Console.ReadKey();
-        }
-    }
-}
+
+        } // END - Main Method
+    
+    } // END - class Program
+
+} // END - namespace Beam.Example.LeaguesToNauticalMiles
