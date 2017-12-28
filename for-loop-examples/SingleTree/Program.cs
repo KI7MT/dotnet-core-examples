67f5898e8bee30bc2d88b5911278e16117c76e83
ki7mt <ki7mt@yahoo.com>
2017-12-27 23:46:13 -0700

    Add Single Tree method and comments

--- for-loop-examples/SingleTree/Program.cs
+++ for-loop-examples/SingleTree/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/* 
+ *  Author ......: Greg, Beam, KI7MT, <ki7mt@yahoo.com>
+ *  Copyright ...: Copyright (C) 2017 GPLv3
+ *  Level .......: Basic
+ *  Target ......: netcoreapp2.0 SingleTree.dll
+ *  Description..: For Loop Single Tree
+ */
+using System;
 
 namespace SingleTree
 {
@@ -6,7 +13,38 @@ namespace SingleTree
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+            // oCount is the outer loop counter, contains two nested loops ( sCount and iCount )
+            // sCount is the spacing loop
+            // iCount is the inner loop which adds the "o's"
+
+            // start outer loop
+            for (int oCount = 1; oCount <= 10; oCount++)
+            {
+                // start first inner loop for spacing
+                for (int sCount = 9; sCount > oCount - 1; sCount--)
+                {
+                    Console.Write(" ");
+                }
+                // end first inner loop
+
+                // start second inner loop for "o"
+                for (int iCount = 1; iCount <= (oCount + oCount - 1); iCount++)
+                {
+                    Console.Write("o", oCount);
+                }
+                // end second inner loop
+
+                Console.WriteLine(); // print line spacing
+            }
+            // end outer loop
+
+            // print footer
+            Console.WriteLine();
+            Console.Write("Press Any Key to Exit...");
+            Console.ReadKey();
+
+        } // END - Main Method
+
+    } // END - class Program
+
+} // END - namespace Beam.Example.SingleTree
