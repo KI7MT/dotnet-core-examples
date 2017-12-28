057e32bf5507370c08380d1f512158ce9f4a6976
ki7mt <ki7mt@yahoo.com>
2017-12-27 23:14:58 -0700

    Add Stack Different and Same code with comments

--- for-loop-examples/StackDifferent/Program.cs
+++ for-loop-examples/StackDifferent/Program.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿/* 
+ *  Author ......: Greg, Beam, KI7MT, <ki7mt@yahoo.com>
+ *  Copyright ...: Copyright (C) 2017 GPLv3
+ *  Level .......: Basic
+ *  Target ......: netcoreapp2.0 StackDifferent.dll
+ *  Description..: For Loop Stack Different Numbers Across
+ */
+using System;
 
-namespace StackDifferent
+namespace Beam.Example.StackDifferent
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+            // start outer loop - count 1 to 9
+            for (int oCount = 1; oCount <= 9; oCount++)
+            {
+               // start inner loop
+               for (int iCount = 1; iCount <= oCount; iCount++)
+               {
+                   Console.Write("{0} ", iCount); // for each outer loop, print value
+               }
+               // end inner loop
+
+               Console.WriteLine(" "); // separates values with space
+            }
+            // end outer loop
+
+            // print footer
+            Console.WriteLine();
+            Console.Write("Press Any Key to Exit..");
+            Console.ReadKey();
+
+        } // END - Main Method
+
+    } // END - class Program
+
+} // END - namespace Beam.Example.StackDifferent
--- for-loop-examples/StackSame/Program.cs
+++ for-loop-examples/StackSame/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/* 
+ *  Author ......: Greg, Beam, KI7MT, <ki7mt@yahoo.com>
+ *  Copyright ...: Copyright (C) 2017 GPLv3
+ *  Level .......: Basic
+ *  Target ......: netcoreapp2.0 StackDifferent.dll
+ *  Description..: For Loop Stack Same Numbers Across
+ */
+using System;
 
 namespace StackSame
 {
@@ -6,7 +13,27 @@ namespace StackSame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+            // start outer loop
+            for (int oCount = 1; oCount <= 9; oCount++)
+            {
+                // start inner loop
+                for (int iCount = 1; iCount <= oCount; iCount++)
+                {
+                    Console.Write("{0} ", oCount);
+                }
+                // end inner loop
+
+                Console.WriteLine(); // end of line return
+            }
+            // end outer loop
+
+            // print footer
+            Console.WriteLine();
+            Console.Write("Press Any Key to Exit...");
+            Console.ReadKey();
+
+        } // END - Main Method
+
+    } // END - class Program
+
+} // END - namespace Beam.Example.StackSame
