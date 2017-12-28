6b51a6e1f4a474e1d17659d89d2d2473b39e5b20
ki7mt <ki7mt@yahoo.com>
2017-12-27 22:27:57 -0700

    Add new structures and update rocket launch

---
+++ for-loop-examples/RocketLaunch/Program.cs
@@ -0,0 +1,35 @@
+﻿/* 
+ *  Author ......: Greg, Beam, KI7MT, <ki7mt@yahoo.com>
+ *  Copyright ...: Copyright (C) 2017 GPLv3
+ *  Level .......: Basic
+ *  Target ......: netcoreapp2.0 RocketLaunch.dll
+ *  Description..: For loop countdown
+ */
+using System;
+
+namespace Beam.Example.RocketLaunch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // start loop
+            for (int tCount = 11 - 1; tCount >= 1; tCount--)
+            {
+                Console.WriteLine();
+                Console.Write(" Rocket launch in {0} seconds", tCount);
+            }
+
+            // print result
+            Console.WriteLine("\n\n Blast Off!!");
+            Console.WriteLine();
+
+            // print footer
+            Console.Write(" Press Any Key to Exit...");
+            Console.ReadKey();
+
+        } // END - Main Method
+
+    } // END - class Program
+
+} // END - namespace Beam.Example.RocketLaunch
---
+++ for-loop-examples/SingleTree/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SingleTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
