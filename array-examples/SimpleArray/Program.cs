c837ef6ae7375909ac387033585abc294c26e773
ki7mt <ki7mt@yahoo.com>
2017-12-28 00:22:55 -0700

    Add Simple Array and comments

---
+++ array-examples/SimpleArray/Program.cs
@@ -0,0 +1,54 @@
+﻿/* 
+ *  Author ......: Greg, Beam, KI7MT, <ki7mt@yahoo.com>
+ *  Copyright ...: Copyright (C) 2017 GPLv3
+ *  Level .......: Basic
+ *  Target ......: netcoreapp2.0 SimpleArray.dll
+ *  Description..: Simple 10 Element Array
+ */
+using System;
+using System.Collections.Generic;
+
+namespace Beam.Example.SimpleArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // method variables
+            double[] listValue = new double[10];
+
+            // print header
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine(" Enter Ten Values For an Array.");
+
+            // start loop - enter values into array
+            Console.WriteLine();
+            for (int iCount = 0; iCount < 10; iCount++)
+            {
+                Console.Write(" Enter Value ..: ");
+                listValue[iCount] = Convert.ToDouble(Console.ReadLine());
+            }
+            // end loop
+
+            // print array values in reverse order
+            Console.WriteLine("");
+            Console.WriteLine(" The values in reverse order are:");
+
+            // start loop
+            for (int oCount = 10 - 1; oCount >= 0; oCount--)
+            {
+                Console.WriteLine(" {0}", listValue[oCount]);
+            }
+            // end loop
+
+            // print footer
+            Console.WriteLine();
+            Console.Write(" Press Any Key to Exit ... ");
+            Console.ReadKey();
+
+        } // END - Main Method
+
+    } // END - class Program
+
+} // END - namespace Beam.Example.SimpleArray
