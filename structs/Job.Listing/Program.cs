6515e85036e050cc58da9ca39aafbcf4f643a674
ki7mt <ki7mt@yahoo.com>
2017-12-29 00:59:06 -0700

    Add initial Job Listing Struct

---
+++ structs/Job.Listing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Job.Listing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
