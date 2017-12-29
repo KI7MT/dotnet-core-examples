fcd067bf2842c817be36537b630f92ab089e64f2
ki7mt <ki7mt@yahoo.com>
2017-12-29 02:09:13 -0700

    Add more advancesd examples

---
+++ Employee.Commision.Advanced/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Employee.Commision.Advanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Employee.Commision.Intermediate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Employee.Commision.Intermediate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
