f15b7784401d7bab3a6340fb4a8a75bbcb545369
ki7mt <ki7mt@yahoo.com>
2017-12-29 02:31:41 -0700

    Re-Add Employee Commission Projects holding Folders

---
+++ Intermediate.Employee.Commision/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Intermediate.Employee.Commision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
