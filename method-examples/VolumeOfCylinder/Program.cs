926c6adf7513488602c3d2c1bdd4e6588758b5df
ki7mt <ki7mt@yahoo.com>
2017-12-28 23:38:22 -0700

    Add Volume of Cylinder and comments

--- method-examples/VolumeOfCylinder/Program.cs
+++ method-examples/VolumeOfCylinder/Program.cs
@@ -1,12 +1,69 @@
-﻿using System;
+﻿/* 
+ *  Author ......: Greg, Beam, KI7MT, <ki7mt@yahoo.com>
+ *  Copyright ...: Copyright (C) 2017 GPLv3
+ *  Level .......: Basic
+ *  Target ......: netcoreapp2.0 AreaOfCylinder.dll
+ *  Description..: Uses a method outside of Main to perform a calculation
+ */
+using System;
 
-namespace VolumeOfCylinder
+namespace Beam.Example.VolumeOfCylinder
 {
     class Program
     {
+        #region Main Method
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // console title
+            Console.Title = "Calculate Area of Triangle";
+
+            // clear screen
+            Console.Clear();
+
+            // method vaariables
+            double inputDiameter;
+            double inputHeight;
+            double returnArea;
+
+            // print header
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine(" Calculate the Volume of a Cylinder");
+            Console.WriteLine("  1. Enter Cylinder Diameter");
+            Console.WriteLine("  2. Enter Cylinder Height");
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine();
+
+            // user input - diameter
+            Console.Write(" Input Cylinder Diameter ..: ");
+            inputDiameter = Convert.ToDouble(Console.ReadLine());
+
+            // user input - height
+            Console.Write(" Input Cylinder Height ....: ");
+            inputHeight = Convert.ToDouble(Console.ReadLine());
+
+            // calculation by calling method
+            returnArea = CalcCylinderVolume(inputDiameter, inputHeight);
+            Console.WriteLine(" Volume of Cylinder .......: {0}", returnArea);
+
+            // print footer
+            Console.WriteLine();
+            Console.Write(" Press Any Key to Exit ... ");
+            Console.ReadKey();
+
         }
-    }
-}
+
+        #endregion
+
+        #region Method: Volume of a Cylinder
+        static double CalcCylinderVolume(double valueDiameter, double valueHeight, double valuePI = Math.PI)
+        {
+            // Formula: Volume = π * (Radius Squared) * h
+            return valuePI * Math.Pow(valueDiameter / 2, 2) * valueHeight;
+        }
+
+        #endregion
+
+    } // END - class Program
+
+} // END - namespace Beam.Example.VolumeOfCylinder
